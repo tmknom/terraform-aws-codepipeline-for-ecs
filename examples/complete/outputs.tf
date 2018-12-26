6e5cdce0b2ce7e494e8a416cf37c0f10e87bed70
tmknom <tmk.nom@gmail.com>
2018-12-26 17:58:10 +0900

    Implement complete example

--- examples/complete/outputs.tf
+++ examples/complete/outputs.tf
@@ -1,1 +1,59 @@
+output "codepipeline_id" {
+  value = "${module.codepipeline.codepipeline_id}"
+}
 
+output "codepipeline_arn" {
+  value = "${module.codepipeline.codepipeline_arn}"
+}
+
+output "codepipeline_webhook_id" {
+  value = "${module.codepipeline.codepipeline_webhook_id}"
+}
+
+output "codepipeline_webhook_url" {
+  value = "${module.codepipeline.codepipeline_webhook_url}"
+}
+
+output "iam_role_arn" {
+  value = "${module.codepipeline.iam_role_arn}"
+}
+
+output "iam_role_create_date" {
+  value = "${module.codepipeline.iam_role_create_date}"
+}
+
+output "iam_role_unique_id" {
+  value = "${module.codepipeline.iam_role_unique_id}"
+}
+
+output "iam_role_name" {
+  value = "${module.codepipeline.iam_role_name}"
+}
+
+output "iam_role_description" {
+  value = "${module.codepipeline.iam_role_description}"
+}
+
+output "iam_policy_id" {
+  value = "${module.codepipeline.iam_policy_id}"
+}
+
+output "iam_policy_arn" {
+  value = "${module.codepipeline.iam_policy_arn}"
+}
+
+output "iam_policy_description" {
+  value = "${module.codepipeline.iam_policy_description}"
+}
+
+output "iam_policy_name" {
+  value = "${module.codepipeline.iam_policy_name}"
+}
+
+output "iam_policy_path" {
+  value = "${module.codepipeline.iam_policy_path}"
+}
+
+output "iam_policy_document" {
+  value = "${module.codepipeline.iam_policy_document}"
+}
--- examples/complete/providers.tf
+++ examples/complete/providers.tf
@@ -1,1 +1,9 @@
+provider "aws" {
+  region = "ap-northeast-1"
+}
 
+# https://www.terraform.io/docs/providers/github/index.html
+provider "github" {
+  token        = "${var.github_token}"
+  organization = "${var.github_organization}"
+}
