e5b9eab2216054742dba1c3b67380ed8331d66c2
tmknom <tmk.nom@gmail.com>
2018-12-26 09:39:51 +0900

    Implement CodePipeline Service Role

--- main.tf
+++ main.tf
@@ -1,3 +1,99 @@
 # Terraform module which creates CodePipeline for ECS resources on AWS.
 #
 # https://docs.aws.amazon.com/codepipeline/latest/userguide/welcome.html
+
+# CodePipeline Service Role
+#
+# https://docs.aws.amazon.com/codepipeline/latest/userguide/how-to-custom-role.html
+
+# https://www.terraform.io/docs/providers/aws/r/iam_role.html
+resource "aws_iam_role" "default" {
+  name               = "${local.iam_name}"
+  assume_role_policy = "${data.aws_iam_policy_document.assume_role_policy.json}"
+  path               = "${var.iam_path}"
+  description        = "${var.description}"
+  tags               = "${merge(map("Name", local.iam_name), var.tags)}"
+}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["codepipeline.amazonaws.com"]
+    }
+  }
+}
+
+# https://www.terraform.io/docs/providers/aws/r/iam_policy.html
+resource "aws_iam_policy" "default" {
+  name        = "${local.iam_name}"
+  policy      = "${data.aws_iam_policy_document.policy.json}"
+  path        = "${var.iam_path}"
+  description = "${var.description}"
+}
+
+data "aws_iam_policy_document" "policy" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "s3:PutObject",
+      "s3:GetObject",
+      "s3:GetObjectVersion",
+      "s3:GetBucketVersioning",
+    ]
+
+    resources = [
+      "arn:aws:s3:::${var.artifact_bucket_name}",
+      "arn:aws:s3:::${var.artifact_bucket_name}/*",
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "codebuild:BatchGetBuilds",
+      "codebuild:StartBuild",
+    ]
+
+    resources = ["*"]
+  }
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "ecs:DescribeServices",
+      "ecs:DescribeTaskDefinition",
+      "ecs:DescribeTasks",
+      "ecs:ListTasks",
+      "ecs:RegisterTaskDefinition",
+      "ecs:UpdateService",
+    ]
+
+    resources = ["*"]
+  }
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "iam:PassRole",
+    ]
+
+    resources = ["*"]
+  }
+}
+
+# https://www.terraform.io/docs/providers/aws/r/iam_role_policy_attachment.html
+resource "aws_iam_role_policy_attachment" "default" {
+  role       = "${aws_iam_role.default.name}"
+  policy_arn = "${aws_iam_policy.default.arn}"
+}
+
+locals {
+  iam_name = "${var.name}-codepipeline-for-ecs"
+}
--- variables.tf
+++ variables.tf
@@ -1,1 +1,27 @@
+variable "name" {
+  type        = "string"
+  description = "The name of the pipeline."
+}
 
+variable "artifact_bucket_name" {
+  type        = "string"
+  description = "The S3 Bucket name of artifacts."
+}
+
+variable "iam_path" {
+  default     = "/"
+  type        = "string"
+  description = "Path in which to create the IAM Role and the IAM Policy."
+}
+
+variable "description" {
+  default     = "Managed by Terraform"
+  type        = "string"
+  description = "The description of the all resources."
+}
+
+variable "tags" {
+  default     = {}
+  type        = "map"
+  description = "A mapping of tags to assign to all resources."
+}
